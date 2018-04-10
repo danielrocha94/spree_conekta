22e872e48312ac09db6b2d1fa3d8d3918302b5c1
daniel.rocha1 <daniel.rocha@verycreatives.com>
2018-04-10 16:11:31 -0500

    line_item unit_price divides between (amount of credit / product count)

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -4,7 +4,7 @@ Spree::LineItem.class_eval do
       :name        => variant.name,
       :description => variant.description,
       :sku         => variant.sku,
-      :unit_price  => ((variant.price + adjustment_total).to_f * 100).to_i,
+      :unit_price  => ((variant.price + adjustment_total).to_f * 100).to_i - ((order.payments.store_credits.valid.last.amount / order.products.count).to_f * 100).ceil,
       :quantity    => quantity
     }
   end
